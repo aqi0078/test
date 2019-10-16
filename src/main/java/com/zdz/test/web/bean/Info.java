804fb5b9afa2ae4fae4e46b97d81b7d8f8ed76ce
aqi0078 <584638114@qq.com>
2019-10-16 11:38:41 +0800

    添加修改名字

--- src/main/java/com/zdz/test/web/bean/Info.java
+++ src/main/java/com/zdz/test/web/bean/Info.java
@@ -23,7 +23,7 @@ public class Info {
     @Column(name = "date_time")
     private Date dateTime;
 
-//    @PrePersist  
+//    @PrePersist    
 //    public void prePersist() {
 //        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
 //        createdAt = timestamp;
